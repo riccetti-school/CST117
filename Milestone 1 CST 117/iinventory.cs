27604d761ee184be362c9a070305783a1b10c192
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-03-07 21:26:34 -0600

    milestone 1

---
+++ Milestone 1 CST 117/iinventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public interface IInventory
+{
+
+    public int id { get; set; }
+
+    public string description { get; set; }
+
+    public string location { get; set; }
+
+    public int quantity { get; set; }
+
+    public bool onOrder { get; set; }
+
+}
