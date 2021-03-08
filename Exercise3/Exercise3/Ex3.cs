8a91e2029ee12c76d9788e5e9f3aad457e400b65
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-03-07 18:59:34 -0600

    exercise 3

---
+++ Exercise3/Exercise3/Ex3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise3
+{
+    public partial class Exercise3 : Form
+    {
+        public Exercise3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var val = textBox1.Text;
+
+            try
+            {
+                var f = float.Parse(val);
+                textBox2.Text = (f / 2f).ToString();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
