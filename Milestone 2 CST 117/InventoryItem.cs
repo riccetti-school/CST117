079ae545b8fd557f8db94b7bdbf6c14450abec5f
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-03-28 18:14:00 -0500

    properties

---
+++ Milestone 2 CST 117/InventoryItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_2_CST_117
+{
+    public class InventoryItem
+    {
+        // id of th item
+        public int Id { get; set; }
+
+        // how many of this item to a package
+        public int PackageSize { get; set; }
+
+        // description of this item
+        public string Description { get; set; }
+
+        // item unit size, individually
+        public float UnitSize { get; set; }
+
+        // unit type, .oz .lbs
+        public string UnitType { get; set; }
+
+        // barcode value
+        public string Barcode { get; set; }
+
+        // is this item damanged
+        public bool Damaged { get; set; }
+
+        public bool Shipped { get; set; }
+
+        // has this item shipped
+        public void Ship(bool shipIt)
+        {
+            Shipped = shipIt;
+        }
+    }
+}
