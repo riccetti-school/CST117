db8595971fd4ab3af09f13b66a9b100645d5e030
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-03-28 10:04:18 -0500

    exercise 6

---
+++ Exercise6/Die.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    public class Die
+    {
+        private int _sides = 6;
+
+        public int Rolls { get; set; } = 0;
+
+        public Die(int sides)
+        {
+            _sides = sides;
+        }
+
+        public void rollDie()
+        {
+            Random rnd = new Random((int)DateTime.Now.Ticks);
+            var t = rnd.Next(1, _sides + 1);
+            while (t != 1)
+            {
+                Rolls++;
+                t = rnd.Next(_sides);
+                Console.WriteLine($"Roll: {Rolls} == {t}");
+                System.Threading.Thread.Yield();
+            }
+
+            Console.WriteLine($"We have a 1!!");
+
+        }
+
+    }
+}
---
+++ Exercise6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // we need two dice
+            var die1 = new Die(6);
+            var die2 = new Die(6);
+
+            die1.rollDie();
+            die2.rollDie();
+
+            var total = die1.Rolls + die2.Rolls;
+
+            Console.WriteLine($"It took {total} rolls to get snake eyes");
+        }
+    }
+}
