8a333998ed2cd0b1d8b28316f08cc63570eea29a
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-04-07 15:56:47 -0500

    first part exercise 7

---
+++ Exercise7/MeasurementConversion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    public class MeasurementConversion
+    {
+
+        public enum ValueType
+        {
+            MilliMeters,
+            Centimeters,
+            Meters,
+            KiloMeters
+        }
+
+        public int ToMeters(ValueType valueType, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ToMilliMeters(ValueType valueType, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ToCentiMeters(ValueType valueType, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ToKiloMeters(ValueType valueType, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Exercise7/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int SumNumbers(int first, int second)
+        {
+            return (first + second);
+        }
+
+        static double AverageNumbers(double first, double second, double third, double fourth, double filth)
+        {
+            return (first + second + third + fourth + filth) / 5;
+        }
+
+        static int RandomSum()
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+
+            return (random.Next() + random.Next());
+        }
+
+        static bool DivisiableByThree(int first, int second, int third)
+        {
+            var t = first + second + third;
+            return t % 3 == 0;
+        }
+
+        static string FewerCharacters(string first, string second)
+        {
+            if (first.Length < second.Length)
+                return first;
+            return second;
+        }
+
+        static double GetLargest(double[] list)
+        {
+            if (list.Length < 1)
+                return 0;
+
+            double j = 0;
+
+            foreach(var i in list)
+            {
+                if (i > j)
+                    j = i;
+            }
+
+            return j;
+        }
+    }
+}
