5fc25876febc912b1ab6fe5f215f3de6366e365e
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-04-08 06:47:42 -0500

    exercise9

---
+++ Exercise9/Program.cs
@@ -0,0 +1,35 @@
+﻿// corrected by Dominic Riccetti
+
+using System;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+
+        }
+    }
+}
---
+++ Exercise9/Set.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    public class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+        private bool containsElement(int val)
+        {
+
+            // I would use the contains function for the list
+            return elements.Contains(val);
+
+            //for (int i = 0; i < elements.Count; i++)
+            //{
+            //    if (val == elements[i])
+            //        return true;
+            //    else
+            //        return false;
+            //}
+            //return false;
+        }
+
+        public override string ToString()
+        {
+
+            // I would also like to sort the elements
+            elements.Sort();
+
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        public Set union(Set rhs)
+        {
+
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                // we need to make sure there isn't a duplicate
+                //this.addElement(rhs.elements[i]);
+                if (!elements.Contains(rhs.elements[i]))
+                    this.addElement(rhs.elements[i]);
+            }
+            return rhs;
+        }
+
+    }
+}
