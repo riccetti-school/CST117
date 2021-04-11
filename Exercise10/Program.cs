618e42dcf6e45d9c7fe455e858e0a60420fc9f8a
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-04-11 08:04:08 -0500

    exercise 10

---
+++ Exercise10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var textFileData = File.ReadAllText("data.txt");
+            var words = textFileData.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+
+            var count = 0;
+
+            // check each word if then end in t or e
+            foreach(var word in words)
+            {
+                if (word.ToCharArray().Last() == 't' || word.ToCharArray().Last() == 'e')
+                    count++;
+            }
+
+            Console.WriteLine($"There are {6} words that end in a t or e");
+        }
+    }
+}
