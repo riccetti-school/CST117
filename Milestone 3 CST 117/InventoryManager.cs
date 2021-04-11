8e0781923cce2c4322e4b16ab5c62de1aff49f98
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-04-11 08:35:09 -0500

    Milestone 3

---
+++ Milestone 3 CST 117/InventoryManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_3_CST_117
+{
+    public class InventoryManager
+    {
+
+        private InventoryItem[] items = null;
+
+        public void Add(InventoryItem i)
+        {
+            if(items == null)
+            {
+                items = new InventoryItem[1];
+                items[0] = i;
+            }
+            else
+            {
+                var l = items.Length;
+                InventoryItem[] temp = new InventoryItem[l + 1];
+                for(int j=0;j<l;j++)
+                {
+                    temp[j] = items[j];
+                }
+
+                temp[l + 1] = i;
+
+                items = new InventoryItem[l + 1];
+                temp.CopyTo(items, 0);
+            }
+        }
+
+        public void Remove(Guid id)
+        {
+            if (items == null) return;
+
+            if(items.Any(c=>c.Id.ToString() == id.ToString()))
+            {
+                InventoryItem[] temp = new InventoryItem[items.Length - 1];
+                for(int i=0;i<items.Length;i++)
+                {
+                    if(items[i].Id != id)
+                    {
+                        temp[i] = items[i];
+                    }
+                }
+
+                items = new InventoryItem[items.Length - 1];
+                temp.CopyTo(items, 0);            }
+
+        }
+
+        public void Display()
+        {
+            for(int i=0;i<items.Length;i++)
+            {
+                Console.WriteLine(items[i].ToString());
+            }
+        }
+
+        public void Search()
+        {
+
+        }
+
+    }
+}
---
+++ Milestone 3 CST 117/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Milestone_3_CST_117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
