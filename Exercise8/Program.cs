c9996557c1b841e89373258527ff31dbd4f78e61
Dominic Riccetti (asus) <riccettid@cettiworks.com>
2021-04-11 07:54:21 -0500

    exercise 8

---
+++ Exercise8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"If I have 9 fat grams I have {FatCalories(9)} calories..");
+            Console.WriteLine($"If I have 12 carbs I have {CarbCalories(12)} calories");
+        }
+
+        static double FatCalories(double fatGrams)
+        {
+            return (fatGrams * 9);
+        }
+
+        static double CarbCalories(double carbs)
+        {
+            return (carbs * 4);
+        }
+    }
+}
